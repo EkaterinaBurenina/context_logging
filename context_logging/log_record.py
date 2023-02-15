3c7a3e33be18787f4b64910e993d881c0db6a6db
e.burenina <Xsw2#edc>
2023-02-15 13:46:19 +0300

    default current_context is dict

--- context_logging/log_record.py
+++ context_logging/log_record.py
@@ -8,7 +8,7 @@ from .context import current_context
 class ContextLogRecord(logging.LogRecord):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
-        self.context = deepcopy(current_context) or ''
+        self.context = deepcopy(current_context) or {}
 
 
 def setup_log_record() -> None:
